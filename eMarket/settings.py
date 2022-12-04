871c607007bc4496d1a52f3c011eab1110704c1b
sadick <sadickminnih@gmail.com>
2022-12-04 10:30:54 +0100

    changing database to postgres

--- eMarket/settings.py
+++ eMarket/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'eMarket.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tchadmarket',
+        'USER' : 'tchadmarket',
+        'PASSWORD' : 'tchadmarket',
+        'HOST' : '127.0.0.1',
+        'PORT' : '5432',
     }
 }
 
