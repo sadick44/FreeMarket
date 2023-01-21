eb3fcf536141cfee3d99f203dd8a9bab83484347
mtd <tousdial11@gmail.com>
2023-01-21 23:14:28 +0100

    Feature/password reset

--- front-end/urls.py
+++ front-end/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from .views import *
-
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', home, name='home'),
     path('compte', user_register, name="signup"),
     path('connexion', user_login, name='login'),
     path('create-post', post_form, name='post'),
+    path('password_reset', password_reset_request, name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="registration/password_reset_confirm.html"), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), name='password_reset_complete'),      
 ]
--- front-end/views.py
+++ front-end/views.py
@@ -5,7 +5,15 @@ from django.contrib.sites.shortcuts import get_current_site
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib import messages
-
+from django.contrib.auth.forms import PasswordResetForm
+from django.db.models.query_utils import Q
+from django.utils.http import urlsafe_base64_encode
+from django.template.loader import render_to_string
+from django.contrib.auth import get_user_model
+from django.core.mail import send_mail, BadHeaderError
+from django.contrib.auth.tokens import default_token_generator
+#from django.utils.translation import ugettext_lazy as _
+from django.utils.encoding import force_bytes
 from accounts.models import PhoneNumber, Post
 from .forms import  PostForm, UserPhoneNumberForm, UserForm
 
@@ -95,3 +103,38 @@ def post_form(request):
             return HttpResponse('Vous avez réussi à poster votre poste')
 
     return render(request, 'forms/post.html', {'post': post})
+
+def password_reset_request(request):
+    User = get_user_model()
+    if request.method == "POST":
+        password_reset_form = PasswordResetForm(request.POST)
+        if password_reset_form.is_valid():
+            data = password_reset_form.cleaned_data['email']
+            associated_users = User.objects.filter(Q(email=data))
+            if associated_users.exists():
+                for user in associated_users:
+                    subject = "Password Reset Requested"
+                    email_template_name = "registration/password_reset_email.txt"  # noqa
+                    c = {
+                        "email": user.email,
+                        'domain': '127.0.0.1:8000',
+                        'site_name': 'Emarket',
+                        "uid": urlsafe_base64_encode(force_bytes(user.pk)),
+                        "user": user,
+                        'token': default_token_generator.make_token(user),
+                        'protocol': 'http',
+                    }
+                    email = render_to_string(email_template_name, c)
+                    try:
+                        send_mail(subject, email, 'schonefeld.dev@gmail.com',
+                                  [user.email], fail_silently=False)
+                    except BadHeaderError:
+                        return HttpResponse('Invalid header found.')
+                    messages.success(request, 'A message with reset password instructions has been sent to your inbox.')
+                    return redirect("password_reset/done")
+    password_reset_form = PasswordResetForm()
+    context = {
+        'password_reset_form': password_reset_form
+    }
+    return render(request=request, context=context,
+                  template_name="registration/password_reset_form.html")
